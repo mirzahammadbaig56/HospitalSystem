9cad1776dce460fee607e51a9198ce47f59ed4e3
Mirza Hammad Baig <mirzahammadbaig56@gmail.com>
2026-08-12 16:47:33 +0500

    added '/' route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express from 'express';
 import patientRouter from './routes/patient.routes.js'
 import doctorRouter from './routes/doctor.routes.js';
 import appointmentRouter from './routes/appointment.routes.js'
+import ApiResponse from './utils/apiResponse.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).json(new ApiResponse(200, "HospitalSystem API is running"));
+});
 app.use('/api/patients', patientRouter);
 app.use('/api/doctors', doctorRouter);
 app.use('/api/appointments', appointmentRouter);
